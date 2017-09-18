fa2f729dc7436273e6c0f34ef6ee4ad1b3f60377
Raúl Nimo <rauibertnf@gmail.com>
2017-09-19 00:19:22 +0200

    Estilos y estructura de la aplicación

---
+++ www/js/camara.js
@@ -0,0 +1,16 @@
+var app = {
+
+    inicio: function(){
+        this.iniciaFastClick();
+    },
+
+    iniciaFastClick: function(){
+        FastClick.attach(document.body);
+    }
+};
+
+if('addEventListener' in document){
+    document.addEventListener('DOMContentLoaded', function(){
+        app.inicio();
+    }, false);
+}
