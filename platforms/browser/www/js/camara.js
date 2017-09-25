40286b52a7820b97182110ef4b66cd5055dca7f1
Raúl Nimo <rauibertnf@gmail.com>
2017-09-26 00:52:02 +0200

    Introducida nueva funcionalidad con tres filtros

--- platforms/browser/www/js/camara.js
+++ platforms/browser/www/js/camara.js
@@ -2,23 +2,39 @@ var app = {
 
     inicio: function(){
         this.iniciaFastClick();
-        this.iniciaBoton();
+        this.iniciarBotones();
     },
 
     iniciaFastClick: function(){
         FastClick.attach(document.body);
     },
 
-    iniciaBoton: function(){
+    iniciarBotones: function(){
         var buttonAction = document.querySelector('#button-action');
         buttonAction.addEventListener('click', function () {
             app.tomarFoto(Camera.PictureSourceType.CAMERA);
+        });
+
+        var buttonSave = document.querySelector('#button-save');
+        buttonSave.addEventListener('click', function () {
+            alert('Foto guardada en la galeria');
+        });
+
+        var filterButtons = document.querySelectorAll('.button-filter');
+        filterButtons[0].addEventListener('click', function () {
+            app.aplicaFiltro('gray');
+        });
+        filterButtons[1].addEventListener('click', function () {
+            app.aplicaFiltro('negative');
+        });
+        filterButtons[2].addEventListener('click', function () {
+            app.aplicaFiltro('sepia');
         });
     },
 
     tomarFoto: function (pictureSourceType) {
             var opciones = {
-                quality: 90,
+                quality: 100,
                 sourceType: pictureSourceType,
                 destinationType: Camera.DestinationType.DATA_URL,
                 targetWidth: 300,
@@ -46,8 +62,19 @@ var app = {
 
     errorAlTomarFoto: function(mensaje){
         console.log('Fallo al tomar foto o toma cancelada: ' + mensaje);
+    },
+
+    aplicaFiltro: function(filterName){
+        var canvas = document.querySelector('#foto');
+        var context = canvas.getContext('2d');
+        imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+        effects[filterName](imageData.data);
+
+        context.putImageData(imageData, 0, 0);
     }
 };
+var imageData;
 
 if('addEventListener' in document){
     document.addEventListener('DOMContentLoaded', function(){
